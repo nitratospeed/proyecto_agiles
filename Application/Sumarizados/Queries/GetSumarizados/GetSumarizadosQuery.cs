a287dffbb89e52a0dec659bd96dd0f669d544a36
Eduardo Rojas <nitratospeed@gmail.com>
2021-03-29 11:01:40 -0500

    added sumarizado endpoint

---
+++ Application/Sumarizados/Queries/GetSumarizados/GetSumarizadosQuery.cs
@@ -0,0 +1,45 @@
+﻿using Application.Common.Interfaces;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Sumarizados.Queries.GetSumarizados
+{
+    public class GetSumarizadosQuery : IRequest<SumarizadoDto>
+    {
+
+    }
+
+    public class GetSumarizadosQueryHandler : IRequestHandler<GetSumarizadosQuery, SumarizadoDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetSumarizadosQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<SumarizadoDto> Handle(GetSumarizadosQuery request, CancellationToken cancellationToken)
+        {
+            var docentesCount = await _context.Docentes.CountAsync();
+            var cursosCount = await _context.Cursos.CountAsync();
+            var cursosCertificadosCount = await _context.Certificados.CountAsync();
+
+            var result = new SumarizadoDto()
+            {
+                Cursos = cursosCount,
+                Docentes = docentesCount,
+                CursosCertificados = cursosCertificadosCount
+            };
+
+            return result;
+        }
+    }
+}
